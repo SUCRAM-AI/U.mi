6def18c97a75da6aaf019d4da085e9545dacffcc
Maria Cecilia L <maria.cecilia6@academico.ufpb.br>
2025-11-10 20:17:39 -0300

    Adicionando o frontend

--- frontend/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-import React from 'react';
-
-export default function Index() {
-  // Redireciona para a tela de login
-  return <Redirect href="/login" />;
-}
