25e63b9b80c0d9a4a018e79d7e4bb4ad1b6f3e5f
danielduartes <duartedaniel479@gmail.com>
2025-11-10 21:38:14 -0300

    Delete frontend directory

--- frontend/umi/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-import React from 'react';
-// "Vá imediatamente para a rota /login."
-export default function Index() {
-  // O Expo Router irá substituir a tela atual pela rota "/login"
-  return <Redirect href="/cadastro" />;
-}
